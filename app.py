6095b4c8651c188f6aa87e321852c5ba9ebd04be
Mikhail Gudyrin <gudyrmik@gmail.com>
2021-04-21 13:30:18 +0200

    123

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request
 from models import BookRecomendation
+from models.BookRecomendation import Recommender
 
 app = Flask(__name__)
-
+Recommender.load_data()
 
 @app.route('/')
 def books():
@@ -12,7 +13,7 @@ def books():
 @app.route('/books_recommendation', methods=['POST', 'GET'])
 def result():
     if request.method == 'POST':
-        result = BookRecomendation.recommend(request.form['Book title'])
+        result = BookRecomendation.Recommender.recommend(request.form['Book title'])
         return render_template("books_recommendation.html", result=result)
 
 
--- models/BookRecomendation.py
+++ models/BookRecomendation.py
@@ -1,89 +1,105 @@
-def recommend(book_title):
+import pandas as pd
+import numpy as np
 
-    print(f"recommend called with {book_title} param")
 
+class Recommender:
     RATINGS_NUMBER_THRESHOLD = 8
 
-    # import
-    import pandas as pd
-    import numpy as np
+    @staticmethod
+    def create_dataset_for_correlations(book_title):
+        # get author of requested book
+        author_arr = np.unique(
+            Recommender.dataset_lowercase.loc[Recommender.dataset_lowercase['Book-Title'] == book_title]['Book-Author'])
+        author = author_arr[1] if author_arr.any() else ''
 
-    # load ratings
-    ratings = pd.read_csv('datasets\\BX-Book-Ratings.csv', encoding='cp1251', sep=';', warn_bad_lines=False)
-    ratings = ratings[ratings['Book-Rating'] != 0]
+        requested_author_users = Recommender.dataset_lowercase['User-ID'][
+            (Recommender.dataset_lowercase['Book-Title'] == book_title) & (
+                    Recommender.dataset_lowercase['Book-Author'] == author)]
+        requested_author_users = np.unique(requested_author_users.tolist())
 
-    # load books
-    books = pd.read_csv('datasets\\BX-Books.csv', encoding='cp1251', sep=';', error_bad_lines=False, warn_bad_lines=False)
+        # final dataset
+        books_of_requested_author_users = Recommender.dataset_lowercase[
+            (Recommender.dataset_lowercase['User-ID'].isin(requested_author_users))]
 
-    # users_ratigs = pd.merge(ratings, users, on=['User-ID'])
-    dataset = pd.merge(ratings, books, on=['ISBN'])
-    dataset_lowercase = dataset.apply(lambda x: x.str.lower() if (x.dtype == 'object') else x)
+        # Number of ratings per other books in dataset
+        number_of_rating_per_book = books_of_requested_author_users.groupby(['Book-Title']).agg('count').reset_index()
 
-    # get author of requested book
-    author_arr = np.unique(dataset_lowercase.loc[dataset_lowercase['Book-Title'] == book_title]['Book-Author'])
-    author = author_arr[1] if author_arr.any() else ''
+        # select only books which have actually higher number of ratings than threshold
+        books_to_compare = number_of_rating_per_book['Book-Title'][
+            number_of_rating_per_book['User-ID'] >= Recommender.RATINGS_NUMBER_THRESHOLD]
+        books_to_compare = books_to_compare.tolist()
 
-    requested_author_users = dataset_lowercase['User-ID'][
-        (dataset_lowercase['Book-Title'] == book_title) & (dataset_lowercase['Book-Author'] == author)]
-    requested_author_users = np.unique(requested_author_users.tolist())
+        Recommender.ratings_data_raw = books_of_requested_author_users[['User-ID', 'Book-Rating', 'Book-Title']][
+            books_of_requested_author_users['Book-Title'].isin(books_to_compare)]
 
-    # final dataset
-    books_of_requested_author_users = dataset_lowercase[(dataset_lowercase['User-ID'].isin(requested_author_users))]
+        # group by User and Book and compute mean
+        ratings_data_raw_nodup = Recommender.ratings_data_raw.groupby(['User-ID', 'Book-Title'])['Book-Rating'].mean()
 
-    # Number of ratings per other books in dataset
-    number_of_rating_per_book = books_of_requested_author_users.groupby(['Book-Title']).agg('count').reset_index()
+        # reset index to see User-ID in every row
+        ratings_data_raw_nodup = ratings_data_raw_nodup.to_frame().reset_index()
 
-    # select only books which have actually higher number of ratings than threshold
-    books_to_compare = number_of_rating_per_book['Book-Title'][
-        number_of_rating_per_book['User-ID'] >= RATINGS_NUMBER_THRESHOLD]
-    books_to_compare = books_to_compare.tolist()
+        dataset_for_corr = ratings_data_raw_nodup.pivot(index='User-ID', columns='Book-Title', values='Book-Rating')
+        return dataset_for_corr
 
-    ratings_data_raw = books_of_requested_author_users[['User-ID', 'Book-Rating', 'Book-Title']][
-        books_of_requested_author_users['Book-Title'].isin(books_to_compare)]
+    @staticmethod
+    def calculate_correlation(book_title, dataset_for_corr):
 
-    # group by User and Book and compute mean
-    ratings_data_raw_nodup = ratings_data_raw.groupby(['User-ID', 'Book-Title'])['Book-Rating'].mean()
+        # Take out the selected book from correlation dataframe
+        dataset_of_other_books = dataset_for_corr.copy(deep=False)
+        if book_title in dataset_of_other_books:
+            dataset_of_other_books.drop(book_title, axis=1, inplace=True)
 
-    # reset index to see User-ID in every row
-    ratings_data_raw_nodup = ratings_data_raw_nodup.to_frame().reset_index()
+        # empty lists
+        book_titles = []
+        correlations = []
+        avgrating = []
 
-    dataset_for_corr = ratings_data_raw_nodup.pivot(index='User-ID', columns='Book-Title', values='Book-Rating')
+        # corr computation
+        for other_book_title in list(dataset_of_other_books.columns.values):
+            book_titles.append(other_book_title)
+            correlations.append(dataset_for_corr[book_title].corr(dataset_of_other_books[other_book_title]))
+            tab = (Recommender.ratings_data_raw[Recommender.ratings_data_raw['Book-Title'] == other_book_title].groupby(
+                Recommender.ratings_data_raw['Book-Title']).mean())
+            avgrating.append(tab['Book-Rating'].min())
+            # final dataframe of all correlation of each book
+        corr_fellowship = pd.DataFrame(list(zip(book_titles, correlations, avgrating)),
+                                       columns=['book', 'corr', 'avg_rating'])
 
-    result_list = []
-    worst_list = []
+        return corr_fellowship
 
-    # Take out the selected book from correlation dataframe
-    dataset_of_other_books = dataset_for_corr.copy(deep=False)
-    if book_title in dataset_of_other_books:
-        dataset_of_other_books.drop(book_title, axis=1, inplace=True)
+    @staticmethod
+    def load_data():
+        # load ratings
+        ratings_data = pd.read_csv('datasets\\BX-Book-Ratings.csv', encoding='cp1251', sep=';',
+                                   warn_bad_lines=False)
+        ratings_data = ratings_data[ratings_data['Book-Rating'] != 0]
 
-    # empty lists
-    book_titles = []
-    correlations = []
-    avgrating = []
+        # load books
+        books_data = pd.read_csv('datasets\\BX-Books.csv', encoding='cp1251', sep=';',
+                                 error_bad_lines=False,
+                                 warn_bad_lines=False)
 
-    # corr computation
-    for other_book_title in list(dataset_of_other_books.columns.values):
-        book_titles.append(other_book_title)
-        correlations.append(dataset_for_corr[book_title].corr(dataset_of_other_books[other_book_title]))
-        tab = (ratings_data_raw[ratings_data_raw['Book-Title'] == other_book_title].groupby(
-            ratings_data_raw['Book-Title']).mean())
-        avgrating.append(tab['Book-Rating'].min())
-        # final dataframe of all correlation of each book
-    corr_fellowship = pd.DataFrame(list(zip(book_titles, correlations, avgrating)),
-                                   columns=['book', 'corr', 'avg_rating'])
-    corr_fellowship.head()
+        # users_ratigs = pd.merge(ratings, users, on=['User-ID'])
+        dataset = pd.merge(ratings_data, books_data, on=['ISBN'])
+        Recommender.dataset_lowercase = dataset.apply(lambda x: x.str.lower() if (x.dtype == 'object') else x)
 
-    # top 10 books with highest corr
-    result_list.append(corr_fellowship.sort_values('corr', ascending=False).head(10))
+    @staticmethod
+    def recommend(book_title, number_of_results=10):
+        dataset_for_corr = Recommender.create_dataset_for_correlations(book_title)
 
-    # worst 10 books
-    worst_list.append(corr_fellowship.sort_values('corr', ascending=False).tail(10))
+        result_list = []
+        worst_list = []
 
-    # print("Correlation for book:", LoR_list[0])
-    # print("Average rating of LOR:", ratings_data_raw[ratings_data_raw['Book-Title']=='the fellowship of the ring (the lord of the rings, part 1'].groupby(ratings_data_raw['Book-Title']).mean()))
-    rslt = result_list[0]
+        dataset_with_correlations = Recommender.calculate_correlation(book_title, dataset_for_corr)
 
-    print(rslt)
+        # top 10 books with highest corr
+        result_list.append(dataset_with_correlations.sort_values('corr', ascending=False).head(number_of_results))
 
-    return rslt
+        # worst 10 books
+        worst_list.append(dataset_with_correlations.sort_values('corr', ascending=False).tail(number_of_results))
+
+        # print("Correlation for book:", LoR_list[0])
+        # print("Average rating of LOR:", ratings_data_raw[ratings_data_raw['Book-Title']=='the fellowship of the ring (the lord of the rings, part 1'].groupby(ratings_data_raw['Book-Title']).mean()))
+        rslt = result_list[0]
+
+        return rslt
